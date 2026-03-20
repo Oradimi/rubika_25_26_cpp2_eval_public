37e0a70d504950a9da17a81e33c3f70b16aa0d85
Oradimi <loganargouse@gmail.com>
2026-03-20 13:30:26 +0100

    Islands done

--- Islands/Islands.cpp
+++ Islands/Islands.cpp
@@ -17,9 +17,64 @@
  * Implement the function count_islands which returns the total number of islands present in the map.
  */
 
+void checkSides(std::vector<std::vector<char>>& grid, std::vector<std::vector<char>>& checked, int& count, int& i, int& j)
+{
+    int k = j + 1;
+    if (k < grid[i].size() && checked[i][k] == '0' && grid[i][k] == '1')
+    {
+        checked[i][k] = '1';
+        checkSides(grid, checked, count, i, k);
+    }
+
+    k = i + 1;
+    if (k < grid.size() && checked[k][j] == '0' && grid[k][j] == '1')
+    {
+        checked[k][j] = '1';
+        checkSides(grid, checked, count, k, j);
+    }
+
+    k = j - 1;
+    if (k >= 0 && checked[i][k] == '0' && grid[i][k] == '1')
+    {
+        checked[i][k] = '1';
+        checkSides(grid, checked, count, i, k);
+    }
+
+    k = i - 1;
+    if (k >= 0 && checked[k][j] == '0' && grid[k][j] == '1')
+    {
+        checked[k][j] = '1';
+        checkSides(grid, checked, count, k, j);
+    }
+}
+
 int count_islands(std::vector<std::vector<char>>& grid)
 {
-	return -1;
+    int count = 0;
+    std::vector<std::vector<char>> checked = grid;
+    for (std::vector<char>& row : checked)
+        for (char& var : row)
+            var = '0';
+    for (int i = 0; i < grid.size(); ++i)
+    {
+        for (int j = 0; j < grid[i].size(); ++j)
+        {
+            if (checked[i][j] == '1')
+                continue;
+
+            checked[i][j] = '1';
+            if (grid[i][j] == '0')
+                continue;
+
+            if (grid[i][j] == '1')
+            {
+                count++;
+                checkSides(grid, checked, count, i, j);
+            }
+        }
+    }
+        
+	return count;
 }
 
 int main()
